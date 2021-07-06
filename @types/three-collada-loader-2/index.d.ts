19ba85429001bee4b57d244cbb754b0d023bc787
remi <info@remialkemade.nl>
2021-07-06 11:31:08 +0200

    Cleaned up build

--- @types/three-collada-loader-2/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'three-collada-loader-2' {
-	import {Object3D} from "three";
-	export type ColladaReponse = { scene:Object3D	};
-	export default class ColladaLoader {
-		load( url:string, onLoad:(collada:ColladaReponse)=>void, onProgress:Function, onError:Function ):void;
-	}
-}
