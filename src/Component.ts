4585eba8b27c775b8481636483c7f34f284667e7
remi <info@remialkemade.nl>
2021-07-09 11:46:34 +0200

    Component.onDestroy called before listeners are stopped.

--- src/Component.ts
+++ src/Component.ts
@@ -659,10 +659,9 @@ export default class Component {
 	}
 
 	private onModelDestroyed() {
+		this.onDestroy();
 		this.stopListening();
 		this.permanentWatchers.forEach(watcher => this.model.$removeWatcher(watcher));
-
-		this.onDestroy();
 		log.info(`${this} destroyed.`);
 	}
 
